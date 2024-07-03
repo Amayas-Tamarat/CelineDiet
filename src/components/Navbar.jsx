21193c3ac39fcab07d75ee5f7c2b49399c7c7939
gaksss <clement.carlier2@gmail.com>
2024-07-03 21:36:26 +0200

    setup finito

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import logo from "../assets/GamingRoom.png"
+const Navbar = () => {
+  return (
+    <nav className="mt-10">
+       <div>
+        <img className="w-10" src={logo}/>
+       </div>
+       <div>
+
+       </div>
+
+    </nav>
+  )
+}
+
+export default Navbar;
