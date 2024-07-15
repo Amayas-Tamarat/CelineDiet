3d88bbb0473ec97d8968eb5b5b60b982b5a75ea7
gaksss <clement.carlier2@gmail.com>
2024-07-15 12:45:52 +0200

    Changement des couleurs

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,24 @@
-import logo from "../assets/GamingRoom.png";
+import LogoInitiales from "../assets/LogoInitiales.png";
 import { CiMenuBurger } from "react-icons/ci";
 import { IoIosClose } from "react-icons/io";
 import React, { useState } from 'react';
 
 const Navbar = () => {
-  const navLinks = document.querySelector('.nav-links')
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   function onToggleMenu() {
     setIsMenuOpen(!isMenuOpen);
   }
   return (
-    <header className="bg-white font-roboto">
-      <nav className="grid grid-cols-2 justify-items-end items-center w-[92%] mx-auto h-[10vh]">
-        <div className="flex justify-self-start items-center gap-2 text-3xl ">
-          <img className="w-16 " src={logo} />
+    <header className="font-roboto bg-lime">
+      <nav className="grid grid-cols-2 justify-items-end items-center w-[92%] mx-auto h-[13vh]">
+        <div className="flex justify-self-start items-center text-3xl">
+          <img className="w-16 sm:w-20 md:w-24 lg:w-28 xl:w-32" src={LogoInitiales} alt="Logo Initiales" />
           Céline Fougerouse
         </div>
         <div className="flex justify-self-end items-center gap-2">
           <div
-            className={`gap-4 flex md:static absolute bg-white md:min-h-fit right-0 items-center ${
-              isMenuOpen ? "top-[60px] py-2" : "top-[-100%]"
+            className={`gap-4 flex md:static absolute bg-lime md:min-h-fit right-0 items-center ${
+              isMenuOpen ? "top-[90px] py-2" : "top-[-100%]"
             } md:w-auto w-full items-center px-5 transition-all duration-500`}
           >
             <ul className="flex md:flex-row flex-col md:item-end md:gap-[4vw] gap-8">
@@ -46,14 +45,12 @@ const Navbar = () => {
             </a>
           </div>
           <div
-          className="justify-self-end flex items-center text-3xl cursor-pointer md:hidden"
-          onClick={onToggleMenu}
-        >
-          {isMenuOpen ? <IoIosClose /> : <CiMenuBurger />}
+            className="justify-self-end flex items-center text-3xl cursor-pointer md:hidden"
+            onClick={onToggleMenu}
+          >
+            {isMenuOpen ? <IoIosClose /> : <CiMenuBurger />}
+          </div>
         </div>
-        </div>
-
-        
       </nav>
     </header>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      rose: "#F2989F",
+      green: "#C2DB9C",
+      cream: "#F2F4B5",
+      palerose : '#F4E2E3',
+      lime : '#BD5663',
+      white : '#F5F4E1',
+    },
     extend: {
-      fontFamily:{
-      'roboto':["Roboto", "sans-serif"]  
-      }
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+      },
     },
   },
   plugins: [],
-}
+};
