4b9cb5187313031486d1fb883fbf99be18d618a0
gaksss <clement.carlier2@gmail.com>
2024-09-19 15:09:34 +0200

    changement meta

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -20,11 +20,11 @@ const Who = () => {
           <p className="text-grey text-base md:text-lg lg:text-xl leading-relaxed text-justify">
             Forte de mes deux compétences, la Diététique reconnue par un BTS Diététique et le Sport par une licence STAPS option activités physiques adaptées, ma double qualification vous apportera une expertise complète et qualitative.
             <br /><br />
-            Depuis plusieurs années, j'ai obtenu une expérience significative dans le domaine de la diététique (<span className="text-rose font-bold">diététicienne</span> au Centre hospitalier du forez) et dans le domaine sportif (employée <span className="text-rose font-bold">sport santé</span> dans un club d'athlétisme, professeure d'activités physiques adaptées à l'association cœur et santé, professeure de gym douce/aquagym dans des associations et entraineur d'athlétisme et de marche nordique).
+            Depuis plusieurs années, j'ai obtenu une expérience significative dans le domaine de la diététique (<span className="text-rose font-bold">diététicienne</span> au Centre hospitalier du forez) et dans le domaine sportif (employée <span className="text-rose font-bold">sport santé</span> dans un club d'athlétisme, professeure d'activités physiques adaptées à l'association cœur et santé, professeure de gym douce/aquagym dans des associations, entraineur d'athlétisme et de marche nordique).
             <br /><br />
             Je vous propose des <span className="text-rose font-bold">consultations diététiques</span> et/ou des séances pratiques de <span className="text-rose font-bold">sport adapté</span> au sein même de mon cabinet à <span className="font-semibold">Andrézieux-Bouthéon</span> en plein cœur du <span className="font-semibold">Forez</span> (<span className="font-semibold">Loire 42</span>).
             <br /><br />
-            Spécialisée dans le sport pour tous j'accompagne les personnes en situation de <span className="text-rose font-bold">handicap</span> (mental ou physique) désireuses de commencer une activité physique ou de se perfectionner dans un domaine <span className="text-rose font-bold">sportif</span>.
+            Également spécialisée dans le sport pour tous j'accompagne les personnes en situation de <span className="text-rose font-bold">handicap</span> (mental ou physique) désireuses de commencer une activité physique ou de se perfectionner dans un domaine <span className="text-rose font-bold">sportif</span>.
             <br /><br />
             Au plaisir de vous rencontrer
           </p>
