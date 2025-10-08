464034a4025a6737a8ad62eb5dcab57244dd547b
Amayas-Tamarat <amayas.tamarat@gmail.com>
2025-10-08 08:20:52 +0200

    home button fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const Home = () => {
-  return (
-    <div className="relative mt-6 mb-14 flex xl:items-center xl:justify-center sm:items-center sm:justify-center">
-      <picture className="xl:w-[50%] mx-6 flex justify-center items-center">
-        <source
-          media="(min-width: 640px)"
-          srcSet="/LogoGrand.png"
-          type="image/png"
-        />
-        <img
-          src="/Logo.png"
-          alt="Illustration décorative"
-          className="w-full h-auto"
-        />
-      </picture>
-      <div className="absolute inset-0  flex-col items-center justify-center top-[10%]  xl:top-[30%] xl:justify-center  hidden sm:flex">
-        <a
-          href="https://www.doctolib.fr/dieteticien/andrezieux-boutheon/celine-fougerouse"
-          className=" lg:text-2xl absolute lg:top-[80%] lg:left-[46%] md:text-xl md:top-[80%] md:left-[42%] bg-green hover:bg-rose text-white py-2 px-4 rounded font-bold transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 xl:mr-6"
-        >
-          Prendre RDV
-        </a>
-      </div>
-    </div>
-  );
+    return (
+        <div className="relative mt-6 mb-14 flex justify-center items-center">
+            <div className="relative w-full xl:w-[50%] mx-6 flex justify-center items-center">
+                <picture className="w-full flex justify-center items-center">
+                    <source
+                        media="(min-width: 640px)"
+                        srcSet="/LogoGrand.png"
+                        type="image/png"
+                    />
+                    <img
+                        src="/Logo.png"
+                        alt="Illustration décorative"
+                        className="w-full h-auto"
+                    />
+                </picture>
+
+                {/* --- Button perfectly anchored on image --- */}
+                <a
+                    href="https://www.doctolib.fr/dieteticien/andrezieux-boutheon/celine-fougerouse"
+                    className="absolute bottom-[8%] left-1/2 -translate-x-1/2 bg-green hover:bg-rose text-white py-2 px-4 rounded font-bold transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 md:text-xl lg:text-2xl"
+                >
+                    Prendre RDV
+                </a>
+            </div>
+        </div>
+    );
 };
 
 export default Home;
